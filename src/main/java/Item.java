a8fd7cbfd30675a2fa6ba10f92c718198f9737cc
Magnus Christian Larsen <happymansi@hotmail.com>
2021-10-26 11:41:10 +0200

    Tilføjede basal item-funktionalitet

---
+++ src/main/java/Item.java
@@ -0,0 +1,25 @@
+package worldofzuul;
+
+public abstract class Item {
+    private String name;
+    private double weight;
+    private EnergyType energyType;
+
+    public Item(String name, double weight, EnergyType energyType) {
+        this.name = name;
+        this.weight = weight;
+        this.energyType = energyType;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public double getWeight() {
+        return this.weight;
+    }
+
+    public EnergyType getEnergyType() {
+        return this.energyType;
+    }
+}
---
+++ src/main/java/Product.java
@@ -0,0 +1,8 @@
+package worldofzuul;
+
+public class Product extends Item {
+
+    public Product(String name, EnergyType energyType) {
+        super(name, 1.0, energyType);
+    }
+}
