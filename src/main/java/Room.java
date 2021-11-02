4620be346e89e0990e5b250ed20504a35d5ebd50
Magnus Christian Larsen <happymansi@hotmail.com>
2021-11-02 11:16:16 +0100

    getDropOffString fix

--- src/main/java/Room.java
+++ src/main/java/Room.java
@@ -1,11 +1,11 @@
 package worldofzuul;
+
 import java.util.*;
 import java.util.ArrayList;
 import java.util.Set;
 import java.util.HashMap;
 
-public class Room
-{
+public class Room {
     private String description;
     private String name;
     private String dropOff;
@@ -16,37 +16,35 @@ public class Room
         this.description = description;
         this.items = new ArrayList<Item>(Arrays.asList((items)));
     }
-    public Room(String description)
-    {
+
+    public Room(String description) {
         this.description = description;
         exits = new HashMap<String, Room>();
     }
 
-    public Room(String description,String dropOff,String name)
-    {
+    public Room(String description, String dropOff, String name) {
         this.description = description;
         this.dropOff = dropOff;
         exits = new HashMap<String, Room>();
         this.name = name;
     }
 
-    public void setExit(String direction, Room neighbor) 
-
-    {
+    public void setExit(String direction, Room neighbor) {
         exits.put(direction, neighbor);
     }
 
-    public String getShortDescription()
-    {
+    public String getShortDescription() {
         return description;
     }
 
-    public String getLongDescription()
-    {
-        return "Du er " + description + ".\n" + getExitString() + ".\n" + getDropOffString();
+    public String getLongDescription() {
+        return "Du er " + description + ".\n" + getExitString() +
+                (getDropOffString().length() == 0 ? "" : ".\n" + getDropOffString());
     }
 
-    public String getName() { return name; }
+    public String getName() {
+        return name;
+    }
 
     private String getExitString() {
         String returnString = "Udgange:";
@@ -55,9 +53,9 @@ public class Room
             returnString += " " + exit;
         }
         //Udskriv hvad der kan samles i et rum hvis der er noget
-        if (items.size() == 0){
+        if (items.size() == 0) {
             return returnString;
-        }else{
+        } else {
             returnString += "\nFølgende ting kan samles op:\n";
             returnString += getRoomItems();
             return returnString;
@@ -66,15 +64,14 @@ public class Room
 
     }
 
-    public String getDropOffString(){
+    public String getDropOffString() {
         if (getDropoff() != null) {
             return "Du kan placere et produkt i dette rum";
         }
         return "";
     }
 
-    public Room getExit(String direction)
-    {
+    public Room getExit(String direction) {
         return exits.get(direction);
     }
 
@@ -83,34 +80,30 @@ public class Room
     }
 
     //Samle ting op fra rummet
-    public Item getItem(String itemName)
-    {
+    public Item getItem(String itemName) {
         for (int i = 0; i < items.size(); i++) {
-            if (items.get(i).getName().equals(itemName))
-            {
+            if (items.get(i).getName().equals(itemName)) {
                 return items.get(i);
             }
         }
         return null;
     }
-    public void removeItem(String itemName)
-    {
+
+    public void removeItem(String itemName) {
         for (int i = 0; i < items.size(); i++) {
-            if (items.get(i).getName().equals(itemName))
-            {
+            if (items.get(i).getName().equals(itemName)) {
                 items.remove(i);
             }
         }
     }
-    public void addItem(Item newitem)
-    {
+
+    public void addItem(Item newitem) {
         items.add(newitem);
     }
 
     //En beskrivelse af hvad der er i et rum
 
-    public String getRoomItems()
-    {
+    public String getRoomItems() {
         String output = "";
         for (int i = 0; i < items.size(); i++) {
             output += items.get(i).getName() + " ";
